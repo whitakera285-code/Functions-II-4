ecc2d9533c0680a02d3301a2f8dac1b22febb8ef
whitakera285-code <whitakera285@gmail.com>
2026-06-21 20:31:07 +0000

    Longest Word Function

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
 // Write your code below
+function findLongestWord(word1, word2, word3) {
+    var longest = word1;
+
+    if (word2.length > longest.length) {
+        longest = word2;
+    }
+    
+    if (word3.length > longest.length) {
+        longest = word3;
+    }
+
+    return longest;
+}
